ef153f10219c70c6d53bbf5a09aafe1b643fbc2f
Vyachaslau <sokol.v083@gmail.com>
2025-01-06 01:44:09 +0300

    организация работы с .env.local

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,12 @@
 const mariadb = require('mariadb');
+const { DB_URI, DB_USER, DB_PASSWORD, DB_DATABASE } = require('./env');
+
 
 const pool = mariadb.createPool({
-  host: 'localhost',   // или IP сервера MariaDB
-  user: 'root',        // пользователь MariaDB
-  password: 'password', // пароль
-  database: 'sensors',  // база данных, которую создадим
+  host: DB_URI,   // или IP сервера MariaDB
+  user: DB_USER,        // пользователь MariaDB
+  password: DB_PASSWORD, // пароль
+  database: DB_DATABASE,  // база данных, которую создадим
   connectionLimit: 5
 });
 
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
+const { API_PORT } = require('./config/env');
 const express = require('express');
 const bodyParser = require('body-parser');
 const sensorRoutes = require('./routes/sensorRoutes');
 const connectDB = require('./config/db');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = API_PORT || 3000;
 
 // Middleware
 app.use(bodyParser.json());
@@ -16,3 +17,4 @@ app.use('/api/sensors', sensorRoutes);
 // Connect to DB and start server
 connectDB();
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
