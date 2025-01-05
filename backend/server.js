fb249e4a0c16b80e490e0f29d33a592a78c36169
Vyachaslau <sokol.v083@gmail.com>
2025-01-06 01:46:26 +0300

    обработка взаимодействия к БД

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const { API_PORT } = require('./config/env');
 const express = require('express');
 const bodyParser = require('body-parser');
 const sensorRoutes = require('./routes/sensorRoutes');
-const connectDB = require('./config/db');
 
 const app = express();
 const PORT = API_PORT || 3000;
@@ -14,7 +13,5 @@ app.use(bodyParser.json());
 // Routes
 app.use('/api/sensors', sensorRoutes);
 
-// Connect to DB and start server
-connectDB();
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
 
